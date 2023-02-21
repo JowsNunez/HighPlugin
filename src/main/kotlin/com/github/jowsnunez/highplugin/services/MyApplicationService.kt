9dc81a3a28bccb01150132e340bc1765bde8502e
GitHub Action <action@github.com>
2023-02-21 03:32:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/jowsnunez/highplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jowsnunez/highplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jowsnunez.highplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jowsnunez.highplugin.MyBundle
 
 class MyApplicationService {
 
